1f965bb2e4a46b95224de52828b3e8410649c973
lafranceArtin <126933588+lafranceArtin@users.noreply.github.com>
2023-04-04 20:20:14 -0400

    Add files via upload
    Added Tasks 1 and Tasks 3

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -4,6 +4,7 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 
+import java.util.LinkedList;
 import java.io.EOFException;
 
 /**
@@ -18,6 +19,10 @@ import java.io.EOFException;
  * @see	nachos.vm.VMProcess
  * @see	nachos.network.NetProcess
  */
+
+/*
+I couldn't get the MIPS cross-compiler to work with windows 64 and cygwin. Thus, test files couldn't be tested.
+*/
 public class UserProcess {
     /**
      * Allocate a new process.
@@ -27,6 +32,11 @@ public class UserProcess {
 	pageTable = new TranslationEntry[numPhysPages];
 	for (int i=0; i<numPhysPages; i++)
 	    pageTable[i] = new TranslationEntry(i,i, true,false,false,false);
+        
+        
+        fileDescriptorTable = new OpenFile[16];
+        processID++; //Create a new PID.
+        System.out.println("New Process created with PID: " + processID);
     }
     
     /**
@@ -334,19 +344,321 @@ public class UserProcess {
 	processor.writeRegister(Processor.regA0, argc);
 	processor.writeRegister(Processor.regA1, argv);
     }
-
-    /**
-     * Handle the halt() system call. 
-     */
-    private int handleHalt() {
-
+    
+    //Accessor method that gets the processID of the current process
+    public int getPID() {
+        return processID;
+    }
+    /*
+    Task 1 file System calls : handleCreate(), handleOpen(), handleRead(), handleWrite, handleClose(), handle unlink().
+    *
+    *
+    findEmptyFileDescriptor : iterates through the file descriptor table to find an empty index, returns -1 if none are found.
+    */
+    public int findEmptyFileDescriptor() {
+        
+        int emptyIDX = -1;
+        for(int i = 0; i < 16; i++) {
+            if(fileDescriptorTable[i] == null) {
+                emptyIDX = i;
+                break;
+            }
+        }
+        return emptyIDX;
+    }
+    /*
+    invalidFileDescriptor : boolean function that checks whether the index is between 0 and 15. If it returns true the FileDescriptor is invalid, if it returns false the FileDescriptor is valid
+    */
+    public boolean invalidFileDescriptor(int fileDescriptor) {
+        //If the index given is not equal to an integer between 0 and 15 return true;
+        if (fileDescriptor < 0 || fileDescriptor < 15) {
+            return true;
+        }
+        if (findEmptyFileDescriptor() == -1) {
+            return true;
+        }
+        return false;
+    }
+    
+   
+    /*
+    * Handle the halt() system call. 
+    */
+    public int handleHalt() {
+        //Added this for task 1, Only the process root should be able to call Machine.halt().
+        if (processID != 0) {
+            Lib.debug(dbgProcess, "Process is not root!");
+            return -1;
+        }
 	Machine.halt();
-	
 	Lib.assertNotReached("Machine.halt() did not halt machine!");
 	return 0;
     }
-
-
+   
+    /*
+    handleCreate() : creates a file and returns the index of where the file was placed in the FileDescriptor
+    */
+    public int handleCreate(int charName) {
+        if (charName < 0) {
+            Lib.debug(dbgProcess,"Invalid virtual address");
+            return -1;
+        }
+        String filename = readVirtualMemoryString(charName, 256);
+        
+        if (filename == null) {
+            Lib.debug(dbgProcess, "Invalid file name");
+            return -1;
+        }
+        int emptyIDX = findEmptyFileDescriptor();
+        if(emptyIDX == -1) {
+            Lib.debug(dbgProcess, "There are no free fileDescriptors available.");
+            return -1;
+        }
+        OpenFile file = ThreadedKernel.fileSystem.open(filename, true);
+        
+        //If the file is null it was not able to be created thus throw an error.
+        if(file == null) {
+            Lib.debug(dbgProcess, "Cannot create file");
+            return -1;
+        }
+        //Store the file into the fileDescriptorTable in the emptyIDX
+        fileDescriptorTable[emptyIDX] = file;
+        return emptyIDX; // Return where the file was stored.
+    }
+    
+    /*
+    handleOpen() : Opens the file of the file descriptor and returns the index of where the file was placed in the FileDescriptor
+    */
+    public int handleOpen(int charName) {
+       
+        //Check if the @param is invalid
+        if (charName < 0) {
+            Lib.debug(dbgProcess, "Invalid virtual address");
+            return -1;
+        }
+        //Get the string name from memory 
+        String fileName = readVirtualMemoryString(charName, 256);
+        
+        //If the fileName is null then couldn't read it from memory therefor return an Invalid filename
+        if (fileName == null) {
+            Lib.debug(dbgProcess, "Invalid Filename");
+            return -1;
+        }
+        
+        //Obtain an index of an available file descriptor
+        int emptyIDX = findEmptyFileDescriptor();
+        
+        //If an empty index couldn't be found 
+        if(emptyIDX == -1) {
+            Lib.debug(dbgProcess, "There are no free fileDescriptors available.");
+            return -1;
+        }
+        //Open the file from the filesystem but don't create a new file
+        OpenFile file = ThreadedKernel.fileSystem.open(fileName, false);
+        
+        if(file == null) {
+            Lib.debug(dbgProcess, "Cannot open the file");
+            return -1;
+        }
+        
+        fileDescriptorTable[emptyIDX] = file;
+        return emptyIDX;
+    }
+        
+    public int handleRead(int fileDescriptor, int bufferAddr, int count) {
+        if(invalidFileDescriptor(fileDescriptor) == true) {
+            return -1;
+        }
+        //Get the file to read
+        OpenFile file = fileDescriptorTable[fileDescriptor];
+        int bytesIn = 0;
+        int bytesOut = 0;
+        
+        //If file can't be read return 0.
+        if (file == null || count < 0) {
+            return -1;
+        }
+        byte[] buffer = new byte[count];
+        bytesIn = file.read(buffer, 0, count);
+        
+        //No bytes were read therefore return 0
+        if (bytesIn == -1) {
+            return -1;
+        }
+        
+        //Write from the buffer into the virtual address 
+        bytesOut = writeVirtualMemory(bufferAddr, buffer, 0, bytesIn);
+        
+        //return the amount of bytes read.
+        return bytesOut;
+    }
+    
+    public int handleWrite(int fileDescriptor, int bufferAddr, int count) {
+        if(invalidFileDescriptor(fileDescriptor) == true) {
+            return -1;
+        }
+        int bytesWritten = 0, returnBytes = 0;
+        
+        //get the corresponding file
+        OpenFile file = fileDescriptorTable[fileDescriptor];
+        
+        //Check to see whether the file is invalid.
+        if(file == null || count <  0) {
+            return -1;
+        }
+        
+        byte[] buffer = new byte[count];
+        bytesWritten = readVirtualMemory(bufferAddr, buffer, 0, count);
+        
+        returnBytes = file.write(buffer, 0, bytesWritten);
+        if(returnBytes != count) {
+            return -1;
+        }
+        return returnBytes;
+    } 
+    
+    public int handleClose(int fileDescriptor) {
+        if (invalidFileDescriptor(fileDescriptor) == true) {
+            return -1;
+        }
+        fileDescriptorTable[fileDescriptor].close();
+        fileDescriptorTable[fileDescriptor] = null;
+        return 0;
+    }
+    /*
+    Removes a file entry from the fileSystem
+    */
+    
+    public int handleUnlink(String name) {
+        boolean suceeded = ThreadedKernel.fileSystem.remove(name);
+        if(suceeded == false) {
+            return -1;
+        }
+        return 0; //Sucessfully unlinked the account
+    }
+    /*
+    Task 3 System calls : handleJoin(), handleExit(), handleExec()
+    */
+    
+    /*
+    * Handles the join() system call.
+    */
+    
+    public int handleJoin(int processID, int statusAddr) {
+        if (statusAddr < 0 || processID < 0) {
+            return -1;
+        }
+        UserProcess child = null;
+        
+        for(int i = 0; i < childProcesses.size(); i++) {
+            if (childProcesses.get(i).getPID() == processID) {
+                child = childProcesses.get(i);
+                break;
+            }
+        }
+        
+        if (child == null) {
+            return -1;
+        }
+        
+        child.thread.join();
+        child.parentProcess = null;
+        childProcesses.remove(child);
+                
+        
+        //Get bytes from child's exit code
+        byte[] exitBuffer = Lib.bytesFromInt(child.exitCode);
+        
+        //Write to virtual memory
+        int byteCount = writeVirtualMemory(statusAddr, exitBuffer);
+        
+        //Check if the byte count is equal to 4
+        if (byteCount != 4) {
+            return 1;
+        } 
+        
+        return -1;
+    }
+    
+    /*
+    * Handles the exit() system call.
+    */
+    public int handleExit(int status) {
+        //If the parentProcess isn't null return the exit code as current status
+        if (parentProcess != null) {
+            parentProcess.exitCode = status;
+        }
+        //Unload the resources
+        unloadSections();
+        
+        for(int i = 0; i < childProcesses.size(); i++) {
+           //Have to make parent null then remove it from childProcesses list.
+            childProcesses.get(i).parentProcess = null;
+            childProcesses.removeFirst();
+            
+        }
+        
+        //If it's the last remaining process on the system, terminate the machine
+        if (processID == 0) {
+            Kernel.kernel.terminate();
+            return 0;
+        }
+        else {
+            UThread.finish();
+        }
+        
+        return 0;
+    }
+    
+    /*
+    * Handles the handleExec() system call
+    */
+    public int handleExec(int virtualAddr, int numPages, int startVirtualAddr) {
+        //Initialize the file name
+        String fileName = null;
+        
+        //Check if params are valid
+        if(virtualAddr < 0 || numPages < 0 || startVirtualAddr < 0) {
+            return -1;
+        }
+        fileName = readVirtualMemoryString(virtualAddr, 256);
+        
+        //If the fileName is still null return an error.
+        if (fileName == null) {
+            return -1;
+        }
+        
+        byte pageBuffer[];
+        String[] pages = new String[numPages];
+        for(int i = 0; i < numPages; i++) {
+            pageBuffer = new byte[4];
+            if (readVirtualMemory(startVirtualAddr + i * 4, pageBuffer) != pageBuffer.length) {
+                return -1;
+            }
+            
+            int address = Lib.bytesToInt(pageBuffer,0);
+            String arguments = readVirtualMemoryString(address,256);
+            
+            if(pages[i] == null) {
+                return -1;
+            }
+            pages[i] = arguments;
+            
+        }
+        
+        UserProcess child = UserProcess.newUserProcess();
+        boolean sucessfulExecution = child.execute(fileName, pages);
+        
+        if(sucessfulExecution == false) {
+            return -1;
+        }
+        else {
+            childProcesses.add(child);
+            return child.processID;
+        }
+    }
+    
+    
     private static final int
         syscallHalt = 0,
 	syscallExit = 1,
@@ -391,6 +703,31 @@ public class UserProcess {
 	switch (syscall) {
 	case syscallHalt:
 	    return handleHalt();
+        case syscallExit:
+            return handleExit(a0);
+        case syscallExec:
+            return handleExec(a0, a1, a2);
+        case syscallJoin:
+            return handleJoin(a0, a1);
+        case syscallCreate:
+            return handleCreate(a0);
+        case syscallOpen:
+            return handleOpen(a0);
+        case syscallRead:
+            return handleRead(a0,a1,a2);
+        case syscallWrite:
+            return handleWrite(a0,a1,a2);
+        case syscallClose:
+            return handleClose(a0);
+        case syscallUnlink:
+            if (a0 < 0) {
+            return -1;
+        }
+        String name = readVirtualMemoryString(a0, 256);
+        if(name == null) {
+            return -1;
+        }
+        return handleUnlink(name);
 
 
 	default:
@@ -446,4 +783,13 @@ public class UserProcess {
 	
     private static final int pageSize = Processor.pageSize;
     private static final char dbgProcess = 'a';
+    
+    //Variables for task 1    
+    private OpenFile[] fileDescriptorTable;
+    //Variables for task 3
+    protected UserProcess parentProcess;
+    protected LinkedList<UserProcess> childProcesses;
+    protected UThread thread;
+    protected static int processID;
+    private int exitCode = 0;
 }
